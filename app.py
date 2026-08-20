ce660cab22d58a5b3e5eb2342255e26b35251c6f
Shubham Kalekar <109839961+shubhya2902@users.noreply.github.com>
2026-08-20 18:27:58 +0530

    Update sender email from noreply to skillcrafttsolutions

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ resend.api_key = RESEND_API_KEY
 
 SENDER_EMAIL = os.environ.get(
     'SENDER_EMAIL',
-    'noreply@skillcraftsolutions.in'
+    'skillcrafttsolutions@gmail.com'
 )
 
 # Model for Enquiry
